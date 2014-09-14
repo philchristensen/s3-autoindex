40ca16939afa40f47fd253db6516e43de8340330
Phil Christensen <pchristensen@freelancersunion.org>
2014-09-14 18:21:30 -0400

    started sketching out real directory listings

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+function listObjectsHandler(err, data) {
+    if (err) {
+        $('.directory-index').prepend($('<p class="error">ERROR: ' + err + '</p>'));
+    } else {
+        var tree = getDirectoryMetadata(data);
+        var sorted_keys = Object.keys(tree);
+        sorted_keys.sort();
+        for(var index in sorted_keys){
+            var key = sorted_keys[index];
+            var value = tree[sorted_keys[index]];
+            if(! value.Key){
+                key += "/"
+            }
+            var row = $('<div class="row"><div class="col-xs-10">' + key + '</div><div class="col-xs-2"></div></div>');
+            $('.directory-index').prepend(row);
+        }
+    }
+}
+
+function getDirectoryMetadata(data){
+    var tree = {};
+    data.Contents.forEach(function (obj) {
+        var path = obj.Key.split('/');
+        var node = tree;
+        for(index in path){
+            var segment = path[index];
+            if(index == path.length - 1){
+                node[segment] = obj;
+            }
+            else {
+                if(tree[segment] === undefined){
+                    node[segment] = {}
+                }
+                node = node[segment];
+            }
+        }
+    });
+    return tree;
+}
