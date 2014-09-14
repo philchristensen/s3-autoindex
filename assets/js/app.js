96ee8603fc363f668e10609a4aec8ee0fef4be0e
Phil Christensen <pchristensen@freelancersunion.org>
2014-09-14 19:50:51 -0400

    basic browsing working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,58 @@
 function listObjectsHandler(err, data) {
     if (err) {
         $('.directory-index').prepend($('<p class="error">ERROR: ' + err + '</p>'));
-    } else {
-        var tree = getDirectoryMetadata(data);
-        var sorted_keys = Object.keys(tree);
-        sorted_keys.sort();
-        for(var index in sorted_keys){
-            var key = sorted_keys[index];
-            var value = tree[sorted_keys[index]];
-            if(value.Key){
-                key = "<a href=\"/" + value.Key + "\">" + key + "</a>";
-            }
-            else {
-                key += "/";
-            }
-            var row = $('<div class="row"><div class="col-xs-10">' + key + '</div><div class="col-xs-2"></div></div>');
-            $('.directory-index').prepend(row);
-        }
+        return;
     }
+
+    var listing = getDirectoryMetadata(data);
+    var requested_path = location.hash ? location.hash.split('#')[1] : '';
+    var tree = getBranch(listing, requested_path);
+    if(! tree){
+        $('.directory-index').prepend($('<p class="error">ERROR: Path not found: ' + requested_path + '</p>'));
+        return;
+    }
+    
+    var sorted_keys = Object.keys(tree);
+    sorted_keys.sort();
+    sorted_keys.reverse();
+    for(var index in sorted_keys){
+        var key = sorted_keys[index];
+        if(key == '') continue;
+        
+        var value = tree[sorted_keys[index]];
+        if(value.Key){
+            key = "<a href=\"/" + value.Key + "\">" + key + "</a>";
+        }
+        else {
+            var subpath = requested_path + '/' + key;
+            key = "<a href=\"#" + subpath + "\">" + key + "/</a>";
+        }
+        var row = $('<div class="row directory-item"><div class="col-xs-10">' + key + '</div><div class="col-xs-2"></div></div>');
+        $('.directory-index').prepend(row);
+    }
+}
+
+function getBranch(tree, location_hash){
+    var path = location_hash.split('/');
+    var node = tree;
+    for(index in path){
+        var segment = path[index];
+        if(index == 0) continue;
+        
+        if(node[segment] === undefined){
+            return null;
+        }
+        node = node[segment];
+    }
+    return node;
+}
+
+function detectBucketName(){
+    var matches = location.hostname.match(/(.*?)\.s3-website-([-\w]*)\.amazonaws\.com/);
+    if(matches){
+        return matches[1];
+    }
+    return location.hostname;
 }
 
 function getDirectoryMetadata(data){
@@ -31,7 +66,7 @@ function getDirectoryMetadata(data){
                 node[segment] = obj;
             }
             else {
-                if(tree[segment] === undefined){
+                if(node[segment] === undefined){
                     node[segment] = {}
                 }
                 node = node[segment];
